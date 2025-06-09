85a2f6e2ec124cef1a60937e9281dd2564f10c88
Kagiso G. Aphane <164793018+kagisogaphane@users.noreply.github.com>
2025-06-09 15:04:27 +0200

    Updated DatabaseConnection.java file

--- src/extraclassapp/util/DatabaseConnection.java
+++ src/extraclassapp/util/DatabaseConnection.java
@@ -16,8 +16,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/extraclass";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "cyberghost";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
